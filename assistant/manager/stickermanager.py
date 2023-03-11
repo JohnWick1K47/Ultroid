a366eec1455c7f0afc0261cede2026767827da70
JohnWick1K47 <69172674+JohnWick1K47@users.noreply.github.com>
2023-03-11 18:48:45 +0530

    Update stickermanager.py

--- assistant/manager/stickermanager.py
+++ assistant/manager/stickermanager.py
@@ -97,7 +97,7 @@ async def kang_cmd(ult):
                     stickers=[SetItem(file, emoji=emoji)],
                     videos=video,
                     animated=animated,
-                    software="@TeamUltroid",
+                    software="@JohnWick1K47",
                 )
             )
         except Exception as er:
